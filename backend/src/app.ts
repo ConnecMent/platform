15b9586237e7a0719c46bc5be23dedd6f0ddc49d
fateme.r <fatemeh.r3366@gmail.com>
2024-01-05 15:21:34 +0330

    Add cors to router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,18 @@ import express from 'express';
 import { apiPort, apiHost } from '@/configs';
 
 import router from './router/v1';
-
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', router);
-
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 const startApp = () => {
   app.listen(apiPort, apiHost, () => {
     console.log(`Service started at http://${apiHost}:${apiPort}`);
