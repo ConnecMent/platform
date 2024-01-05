6ed0e218afe690beac56939a45889471e670b8d4
MahtaMirzaei <mahtamir2004@gmail.com>
2024-01-05 12:11:23 +0330

    just initial files

---
+++ frontend/transferScript.js
@@ -0,0 +1,11 @@
+function transferMent() {
+  from = document.getElementById("userF").value;
+  to = document.getElementById("userT").value;
+  amount = document.getElementById("value").value;
+
+  amount = Math.abs(amount);
+  if(from<amount){
+    
+  }
+
+}
