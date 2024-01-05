3955b126b53a9a418d361ba610aa4c70eb6a3b20
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-01-05 14:33:22 +0330

    update api

--- frontend/headerAPI.js
+++ frontend/headerAPI.js
@@ -1,12 +1,21 @@
 import { apiUrl } from "./config.js";
+const username = localStorage.get("username");
 
 async function headerAPI() {
-  const response = await fetch(`${apiUrl}/profile`).then((j) => {
+  const response = await fetch(`${apiUrl}/info`, {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((j) => {
     return j.json();
   });
 
-  document.getElementById("header-name").textContent = response["name"];
-  document.getElementById("header-token").textContent = response["token"];
+  document.getElementById("header-name").textContent = username;
+  document.getElementById("header-token").textContent = response["balance"];
 }
 
 document.addEventListener("DOMContentLoaded", headerAPI);
