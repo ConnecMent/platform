2cc291d17dd0b0eac792cae0c82ca15e901fdd80
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-01-05 15:20:31 +0330

    remove old index page

--- frontend/main.js
+++ frontend/main.js
@@ -1,24 +1,22 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { apiUrl } from "./config.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function updateCardInfo() {
+  const username = localStorage.getItem("username");
+  const response = await fetch(`${apiUrl}/info`, {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((j) => {
+    return j.json();
+  });
 
-setupCounter(document.querySelector('#counter'))
+  document.getElementById("main-name").textContent = username;
+  document.getElementById("main-token").textContent = response["balance"];
+}
+
+document.addEventListener("DOMContentLoaded", updateCardInfo);
