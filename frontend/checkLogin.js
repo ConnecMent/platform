b67d52ead6e0e56e218fb1174f80bfa13f93e831
Mohammad Kermani <mkermani144@gmail.com>
2024-01-05 18:04:33 +0330

    fix issue in checkLogin

--- frontend/checkLogin.js
+++ frontend/checkLogin.js
@@ -1,8 +1,8 @@
 const urlSplit = window.location.pathname.split("/");
 if (
   urlSplit[urlSplit.length - 1] != "login.html" &&
-  localStorage.getItem("username") == null &&
-  localStorage.getItem("token") == null
+  !localStorage.getItem("username") &&
+  !localStorage.getItem("token")
 ) {
   window.location.href = "./login.html";
 }
