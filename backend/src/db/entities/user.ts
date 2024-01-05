5b39ec90ec68cdc351bfe7828f0a331d5afc5acc
fateme.r <fatemeh.r3366@gmail.com>
2024-01-05 07:49:26 +0000

    backend init

---
+++ backend/src/db/entities/user.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar')
+  username: string;
+
+  @Column('varchar')
+  password: string;
+
+  @Column('integer')
+  balance: number;
+
+  @Column('boolean')
+  isAdmin: boolean;
+}
