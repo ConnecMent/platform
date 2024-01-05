0d7a724437507a87faeae2a5da537d1bcd01588d
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-01-05 14:33:22 +0330

    update api

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -1,13 +1,21 @@
 import { apiUrl } from "./config.js";
+const username = localStorage.get("username");
 
 async function updateCardInfo() {
-    const response = await fetch(`${apiUrl}/profile`).then((j) => {
-      return j.json();
-    });
-  
-    document.getElementById("main-name").textContent = response["name"];
-    document.getElementById("main-token").textContent = response["token"];
-  }
-  
-  document.addEventListener("DOMContentLoaded", updateCardInfo);
-  
+  const response = await fetch(`${apiUrl}/info`, {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((j) => {
+    return j.json();
+  });
+
+  document.getElementById("main-name").textContent = username;
+  document.getElementById("main-token").textContent = response["balance"];
+}
+
+document.addEventListener("DOMContentLoaded", updateCardInfo);
