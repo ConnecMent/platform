452b0c9a5ea3aa49d6c51c4d33d2d270dd574044
Kiarash <kia.iranpour1380@gmail.com>
2024-01-05 14:19:10 +0330

    login page added to project

---
+++ frontend/login.js
@@ -0,0 +1,102 @@
+import { isUsernameValid, isPasswordValid } from "./utils";
+
+let loginForm = document.getElementById("login-form");
+let usernameInput = document.getElementById("username-input");
+let passwordInput = document.getElementById("password-input");
+let togglePasswordBtn = document.getElementById("toggle-password-btn");
+let usernameInputTouched = false;
+let passwordInputTouched = false;
+
+let errorsObject = {};
+
+usernameInput.addEventListener("input", (e) => {
+  if (usernameInputTouched) {
+    usernameInputValidation(e.target.value);
+  }
+});
+
+usernameInput.addEventListener("blur", (e) => {
+  usernameInputTouched = true;
+  usernameInputValidation(e.target.value);
+});
+
+passwordInput.addEventListener("input", (e) => {
+  if (passwordInputTouched) {
+    passwordInputValidation(e.target.value);
+  }
+});
+
+passwordInput.addEventListener("blur", (e) => {
+  passwordInputTouched = true;
+  passwordInputValidation(e.target.value);
+});
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  usernameInputTouched = true;
+  passwordInputTouched = true;
+  passwordInputValidation(passwordInput.value);
+  usernameInputValidation(usernameInput.value);
+  console.log(errorsObject);
+  if (isObjEmpty(errorsObject)) {
+    let formData = new FormData(loginForm);
+    var object = {};
+    formData.forEach((value, key) => (object[key] = value));
+    var json = JSON.stringify(object);
+    fetch(base_url);
+  }
+});
+
+togglePasswordBtn.addEventListener("click", () => {
+  let inputType = passwordInput.getAttribute("type");
+
+  if (inputType === "password") {
+    passwordInput.setAttribute("type", "text");
+    togglePasswordBtn.querySelector("img").src = "/public/images/eye.png";
+  } else {
+    passwordInput.setAttribute("type", "password");
+    togglePasswordBtn.querySelector("img").src = "/public/images/eye-slash.png";
+  }
+});
+
+function isObjEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function renderError(container, field) {
+  let errorElement = document.createElement("li");
+  errorElement.textContent = errorsObject[field];
+  container.innerHTML = "";
+  container.append(errorElement);
+}
+
+function removeError(container, field) {
+  container.innerHTML = "";
+  delete errorsObject[field];
+}
+
+function usernameInputValidation(value) {
+  if (!isUsernameValid(value)) {
+    errorsObject[usernameInput.name] =
+      "username must be greater than 6 characters and must not start with a number";
+    renderError(usernameInput.nextElementSibling, usernameInput.name);
+  } else {
+    removeError(usernameInput.nextElementSibling, usernameInput.name);
+  }
+}
+
+function passwordInputValidation(value) {
+  if (!isPasswordValid(value)) {
+    errorsObject[passwordInput.name] =
+      "password must be greater than 5 characters password must containt at least one symbol or number";
+    renderError(
+      passwordInput.parentElement.nextElementSibling,
+      passwordInput.name
+    );
+  } else {
+    removeError(
+      passwordInput.parentElement.nextElementSibling,
+      passwordInput.name
+    );
+  }
+}
